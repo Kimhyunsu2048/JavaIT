fb1292dda5d8f7b679529ac9081d2ddf9ba4aa30
Kimhyunsu2048 <kshbjy@hanmail.net>
2022-11-26 12:54:44 +0900

    자바9일

---
+++ javaKorea/src/day09/DateTime3.java
@@ -0,0 +1,57 @@
+package day09;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import day01.Syso1;
+
+public class DateTime3 {
+	public static void main(String[] args) {
+		MyTimer3 myTimer3 = new MyTimer3();
+		myTimer3.go();
+		
+	}
+}
+
+
+class MyTimer3{
+	private String str;
+	
+	public void go() {
+		String m_year;
+		String m_month;
+		String m_day;
+		
+		Calendar c = Calendar.getInstance(); 	
+		Date date = c.getTime();
+		
+		SimpleDateFormat year = new SimpleDateFormat("yyyy");
+		SimpleDateFormat month = new SimpleDateFormat("MM");
+		SimpleDateFormat day = new SimpleDateFormat("dd");
+		
+		m_year = year.format(date);
+		m_month = month.format(date);
+		m_day = day.format(date);
+		int day_int = Integer.parseInt(m_day);
+		
+		System.out.println(m_year+"년");
+		System.out.println(m_month+"월");
+		System.out.println("내일은 "+m_year+"년 "+m_month+"월"+(day_int+1)+"일 입니다.");		
+		
+	}
+	
+	
+	
+	// String -> int
+	// num = Integer.parseInt(str);
+	
+}
+
+/*
+	2022년
+	11월
+	내일은 2022년 11월 27일 입니다.
+  
+*/
+
